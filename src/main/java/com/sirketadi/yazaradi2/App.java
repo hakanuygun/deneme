f8988c42a29a0c1318cdc1bd858901f30d57dabf
hakan uygun <hakanuygun79@gmail.com>
2024-12-31 00:23:37 +0200

    ilk kommitim

---
+++ src/main/java/com/sirketadi/yazaradi2/App.java
@@ -0,0 +1,13 @@
+package com.sirketadi.yazaradi2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/com/sirketadi/yazaradi2/Deneme.java
@@ -0,0 +1,10 @@
+package com.sirketadi.yazaradi2;
+
+public class Deneme {
+
+	public static void main(String[] args) {
+		System.out.println("Merhabalar.... Git.......");
+
+	}
+
+}
