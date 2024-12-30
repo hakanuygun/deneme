0bced2020e9afc3ef22c7e647aa3cead3d23cfb4
hakan uygun <hakanuygun79@gmail.com>
2024-12-31 01:11:34 +0200

    Commit -3 Yeni Branch

---
+++ src/main/java/com/sirketadi/yazaradi2/DDeneme.java
@@ -0,0 +1,10 @@
+package com.sirketadi.yazaradi2;
+
+public interface DDeneme {
+	
+	public static void main(String[] args) {
+		System.out.println("Merhabalar DDeneme dosyası.... Git.......");
+
+	}
+
+}
